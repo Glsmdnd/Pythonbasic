af6cae6f9d0fee120413d5d0276edfb71cecb618
Glsmdnd <94369360+Glsmdnd@users.noreply.github.com>
2021-12-12 20:01:31 +0300

    Python İç İçe Liste Dön

---
+++ test.py
@@ -0,0 +1,8 @@
+fruits = [["apple", "banana", "cherry"],["apple", "banana"]]
+
+for array in fruits:
+    for fruit in array:
+            if fruit=='banana':
+               continue 
+            else:
+             print(fruit)
